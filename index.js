febf32b34406dda5669b90f6b70d1f6f57c7f3cc
Xapu1337 <xapursplaz@gmail.com>
2020-11-26 20:42:41 +0100

    1

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ client.addWarning = async function (message, reason, user){
 
 client.deleteWarning = async function (message, id){
     const req = await client.db.findOne({id: message.guild.id});
-    req.warnings.splice(req.warnings.indexOf(req.warnings.filter((i) => i.id === id)), 1)
+    req.warnings.splice(req.warnings.removeByValue(req.warnings.filter((i) => i.id === id)))
     req.save();
     await message.channel.send(new MessageEmbed()
         .setColor("RED")
