72ebf1fe8c3c44b137964d3d4e3d7aeb44105e44
Xapu1337 <xapursplaz@gmail.com>
2020-12-04 09:36:43 +0100

    Hxjf

--- commands/fun/dice.js
+++ commands/fun/dice.js
@@ -7,13 +7,12 @@ module.exports = {
     description: "Result an dice msg",
     usage: "",
     permissions: "EVERYONE",
-    run: (client, message, args) => {
+    run: async (client, message, args) => {
     
     const nanoid = customAlphabet('123456', 1)
-        console.log(client.getColorFromImage(message.author.displayAvatarURL({size: 4096, format: "png"})));
     let dice = new MessageEmbed()
     .setTimestamp()
-    .setColor(client.getColorFromImage(message.author.displayAvatarURL({size: 4096, format: "png"})))
+    .setColor(await client.getColorFromImage(message.author.id))
     .setFooter(message.author.username, message.author.displayAvatarURL())
     .setDescription(`🎲 | Your random dice is: ${nanoid()}`)
     message.channel.send(dice);
