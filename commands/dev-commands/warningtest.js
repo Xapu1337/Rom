7767bf6c101cc96f4ed4e2114857515152381889
BuildTools <unconfigured@null.spigotmc.org>
2020-12-12 23:15:13 +0100

    Initial commit

--- commands/dev-commands/warningtest.js
+++ commands/dev-commands/warningtest.js
@@ -11,7 +11,7 @@ module.exports = {
     run: async (client, message, args) => {
         switch (args[0].toLowerCase()){
             case "add":
-                client.addWarning(message, client.extendedMemberSearch(message, args, 1), args.slice(1).join(" ")).then(i => console.log(i));
+                client.addWarning(message, client.extendedMemberSearch(message, args, 1), args.split(1).join(" ")).then(i => console.log(i));
                 break;
             case "remove":
                 client.deleteWarning(message, args[1]).then(i => console.log(i));
@@ -19,10 +19,11 @@ module.exports = {
             case "getwarns":
                 let reasonIdMix = [];
                 let req = await client.getGuildDB(message.guild.id);
-                req.warnings.filter((i) => i.userID === client.extendedMemberSearch(message, args, 1).id).forEach(i => {
+                req.warnings.filter((i) => i.userID === client.extendedMemberSearch(message, args, 0).id).forEach(i => {
                     reasonIdMix.push(i.id+" Reason: \""+i.reason+"\"");
                 });
-                console.log(await client.extendedMemberSearch(message, args, 1).id)
+                console.log(await client.extendedMemberSearch(message, args, 0))
+                console.log(await client.extendedMemberSearch(message, args, 1))
                 await message.channel.send(new MessageEmbed()
                     .addField(`Warns from: ${client.extendedMemberSearch(message, args, 1).username}`, reasonIdMix.join("\n"), true)
                     .setColor(await client.getColorFromImage(await client.extendedMemberSearch(message, args, 1).id))
