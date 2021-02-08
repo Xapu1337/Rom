cc7132d2d6f7f6872daed0c9f5c266af771fd97e
Xapu1337 <xapursplaz@gmail.com>
2021-02-08 10:59:26 +0100

    Initial commit

--- commands/dev-commands/asciiart.js
+++ commands/dev-commands/asciiart.js
@@ -8,6 +8,6 @@ module.exports = {
     usage: "",
     permissions: "AUTHOR",
     run: async (client, message, args) => {
-        message.channel.send(`\`\`\`${args.join(" ").replaceAll("0", " ").replaceAll("1","█")}\`\`\``);
+        message.channel.send(`\`\`\`${args.join(" ").replaceAll("0", " ").replaceAll("1","▄")}\`\`\``);
     }
 }
