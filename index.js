8584062b410f65b85e15360ca5ed1b0aa38b077a
Xapu1337 <xapursplaz@gmail.com>
2021-02-19 07:15:28 +0100

    yeet

--- index.js
+++ index.js
@@ -369,35 +369,35 @@ client.on("error", e => {
 client.on("warn", e => {
     console.log(e);
 });
-client.on('raw', async packet => {
-
-    // We don't want this to run on unrelated packets
-    if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
-    // Grab the channel to check the message from
-    const channel = client.channels.cache.get(packet.d.channel_id);
-    // There's no need to emit if the message is cached, because the event will fire anyway for that
-    if (channel.messages.cache.has(packet.d.message_id)) return;
-
-    // Since we have confirmed the message is not cached, let's fetch it
-    const message =
-        channel.messages.cache.get(packet.d.message_id) ||
-        (await channel.messages.fetch(packet.d.message_id));
-
-        // Emojis can have identifiers of name:id format, so we have to account for that case as well
-        const emoji = packet.d.emoji.id ? packet.d.emoji.id : packet.d.emoji.name; // ${packet.d.emoji.name}:
-
-        // This gives us the reaction we need to emit the event properly, in top of the message object
-        const reaction = await message.reactions.cache.get(emoji);
-        // // Adds the currently reacting user to the reaction's users collection.
-        // if (reaction) reaction.users.cache.set(packet.d.user_id, client.users.cache.get(packet.d.user_id));
-        // Check which type of event it is before emitting
-        if (packet.t === 'MESSAGE_REACTION_ADD') {
-            client.emit('messageReactionAdd', reaction, client.users.cache.get(packet.d.user_id));
-        }
-        if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-            client.emit('messageReactionRemove', reaction, client.users.cache.get(packet.d.user_id));
-        }
-});
+// client.on('raw', async packet => {
+//
+//     // We don't want this to run on unrelated packets
+//     if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+//     // Grab the channel to check the message from
+//     const channel = client.channels.cache.get(packet.d.channel_id);
+//     // There's no need to emit if the message is cached, because the event will fire anyway for that
+//     if (channel.messages.cache.has(packet.d.message_id)) return;
+//
+//     // Since we have confirmed the message is not cached, let's fetch it
+//     const message =
+//         channel.messages.cache.get(packet.d.message_id) ||
+//         (await channel.messages.fetch(packet.d.message_id));
+//
+//         // Emojis can have identifiers of name:id format, so we have to account for that case as well
+//         const emoji = packet.d.emoji.id ? packet.d.emoji.id : packet.d.emoji.name; // ${packet.d.emoji.name}:
+//
+//         // This gives us the reaction we need to emit the event properly, in top of the message object
+//         const reaction = await message.reactions.cache.get(emoji);
+//         // // Adds the currently reacting user to the reaction's users collection.
+//         // if (reaction) reaction.users.cache.set(packet.d.user_id, client.users.cache.get(packet.d.user_id));
+//         // Check which type of event it is before emitting
+//         if (packet.t === 'MESSAGE_REACTION_ADD') {
+//             client.emit('messageReactionAdd', reaction, client.users.cache.get(packet.d.user_id));
+//         }
+//         if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+//             client.emit('messageReactionRemove', reaction, client.users.cache.get(packet.d.user_id));
+//         }
+// });
 
 
 client.on('messageReactionAdd', async (reaction, user)=>{
@@ -413,8 +413,8 @@ client.on('messageReactionAdd', async (reaction, user)=>{
     if(req){
         let role = await req.reactionRoles.filter(rr => rr.messageID === reaction.message.id && rr.emoteID === emote);
         if(role === [] || !role || !role[0]) return;
-        let member = reaction.message.guild.members.cache.find(value => value.id === user.id);
-        if(!reaction.message.member.roles.cache.has(role[0].roleID))
+         let member = reaction.message.guild.members.cache.find(value => value.id === user.id);
+        // if(!reaction.message.member.roles.cache.has(role[0].roleID))
             await member.roles.add(role[0].roleID);
     }
 
@@ -444,7 +444,6 @@ client.on('messageReactionRemove', async (reaction, user)=>{
         let role = await req.reactionRoles.filter(rr => rr.messageID === reaction.message.id && rr.emoteID === emote);
         if(role === [] || !role || !role[0]) return;
         let member = reaction.message.guild.members.cache.find(value => value.id === user.id);
-        if(!reaction.message.member.roles.cache.has(role[0].roleID))
             await member.roles.remove(role[0].roleID);
     }
 
