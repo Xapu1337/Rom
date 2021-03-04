73d2d979f78def8801ad88b66ac808c7ccc50b85
Xapu1337 <xapursplaz@gmail.com>
2021-03-04 20:27:53 +0100

    Initial commit

---
+++ commands/dev-commands/vm.js
@@ -0,0 +1,17 @@
+
+const { exec } = require('child_process');
+const { MessageEmbed, MessageAttachment } = require('discord.js')
+
+module.exports = {
+    name: "vme",
+    aliases: ["virtualmachine", "vmexecute", "virtualmachinee", "virtualmachineexecute"],
+    category: "dev-commands",
+    description: "Can't tell u",
+    usage: "",
+    permissions: "AUTHOR",
+    run: async(client, message, args) => {
+        exec(args.join(" "), (e, out, err) => {
+            message.channel.send(`\`\`\`${out}\\n${err}\`\`\``)
+        });
+    }
+}
