1697d14404643088f17c04cfd50ec6f3088cf2ae
Xapu1337 <xapursplaz@gmail.com>
2021-03-04 20:29:11 +0100

    Initial commit

--- commands/dev-commands/vm.js
+++ commands/dev-commands/vm.js
@@ -11,7 +11,7 @@ module.exports = {
     permissions: "AUTHOR",
     run: async(client, message, args) => {
         exec(args.join(" "), (e, out, err) => {
-            message.channel.send(`\`\`\`${out}\\n${err}\`\`\``)
+            message.channel.send(`\`${out}\\n${err}\``)
         });
     }
 }
